c4ef9921b4f2f08436990d13cdf0c00970a32bee
Doma Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-03 12:27:18 +0530

    Time: 1388 ms (5.11%), Space: 25.5 MB (75.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if not nums:
+            return 
         if k>len(nums):
             k=k-len(nums)
         for i in range(k):
