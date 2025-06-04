3ffbb0be578859e4063ce5220d487ff4d6cf8131
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-04 16:36:27 +0530

    Time: 3 ms (22.53%), Space: 18.1 MB (10.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        count=0
+        sym={'(':')','{':'}','[':']'}
+        st=[]
+        for i in s:
+            if i in sym.keys():
+                st.append(i)
+            if i in sym.values():
+                if not st or sym[st.pop()]!=i:
+                    return False
+        return len(st)==0
+            
