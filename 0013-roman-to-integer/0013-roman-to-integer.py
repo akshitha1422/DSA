43097e5cdb8bfe2c8e27310d5bf793f99f9b3cc1
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-04 15:53:17 +0530

    Time: 2 ms (83.97%), Space: 17.9 MB (20.54%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        total=0
+        sym={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        i=0
+        while i<len(s):
+            if i<(len(s)-1) and s[i]=='I' and (s[i+1]=='V' or s[i+1]=='X'):
+                total+=sym[s[i]+s[i+1]]
+                i+=2
+            elif i<(len(s)-1) and s[i]=='X' and (s[i+1]=='L' or s[i+1]=='C'):
+                total+=sym[s[i]+s[i+1]]
+                i+=2
+            elif i<(len(s)-1) and s[i]=='C' and (s[i+1]=='D' or s[i+1]=='M'):
+                total+=sym[s[i]+s[i+1]]
+                i+=2
+            else:
+                total+=sym[s[i]]
+                i+=1
+        return total
+    
