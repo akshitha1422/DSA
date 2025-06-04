3c847fac6c8b037019c5a1818ab3fd2b2633e568
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-04 22:52:47 +0530

    Time: 0 ms (100%), Space: 18 MB (9.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        exp=0
-        s=0
-        for i in reversed(digits):
-            s+=i*(10**exp)
-            exp+=1
-        s=s+1
-        new=[]
-        while s>0:
-            a=s%10
-            new.insert(0,a)
-            s=s//10
-        return new
+        # exp=0
+        # s=0
+        # for i in reversed(digits):
+        #     s+=i*(10**exp)
+        #     exp+=1
+        # s=s+1
+        # new=[]
+        # while s>0:
+        #     a=s%10
+        #     new.insert(0,a)
+        #     s=s//10
+        # return new
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+digits
