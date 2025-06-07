618b8bce6332bbacd573fceaa07efb7a2152bdc3
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-07 11:47:46 +0530

    Time: 3 ms (22.85%), Space: 18 MB (7.73%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,7 +1,20 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1+str2!=str2+str1:
-            return ''
+        if len(str1)<len(str2):
+            word=str1
         else:
-            gcd=math.gcd(len(str1),len(str2))
-        return str1[:gcd]
+            word=str2
+        sub=''
+        ans=''
+        for i in word:
+            sub=sub+i
+            if str1==sub*(len(str1)//len(sub)) and str2==sub*(len(str2)//len(sub)):
+                ans=sub
+        return ans
+
+
+        # if str1+str2!=str2+str1:
+        #     return ''
+        # else:
+        #     gcd=math.gcd(len(str1),len(str2))
+        # return str1[:gcd]
