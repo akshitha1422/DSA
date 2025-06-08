43169e2a74d417d60b89245e3dcfb7ae419e3c94
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-08 16:53:10 +0530

    Time: 3 ms (84.02%), Space: 21.3 MB (90.36%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,19 +1,23 @@
 class MinStack:
 
     def __init__(self):
+        self.Stack=[]
         self.MinStack=[]
 
     def push(self, val: int) -> None:
-        self.MinStack.append(val)
+        self.Stack.append(val)
+        if not self.MinStack or val<=self.MinStack[-1]:
+            self.MinStack.append(val)
 
     def pop(self) -> None:
-        self.MinStack.pop()
+        if self.Stack.pop()==self.MinStack[-1]:
+            self.MinStack.pop()
 
     def top(self) -> int:
-        return self.MinStack[-1]
+        return self.Stack[-1]
 
     def getMin(self) -> int:
-        return min(self.MinStack)
+        return self.MinStack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -22,3 +26,21 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+
+# class MinStack:
+
+#     def __init__(self):
+#         self.MinStack=[]
+
+#     def push(self, val: int) -> None:
+#         self.MinStack.append(val)
+
+#     def pop(self) -> None:
+#         self.MinStack.pop()
+
+#     def top(self) -> int:
+#         return self.MinStack[-1]
+
+#     def getMin(self) -> int:
+#         return min(self.MinStack)
