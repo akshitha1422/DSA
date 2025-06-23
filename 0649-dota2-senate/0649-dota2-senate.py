f366398ec8fa6440e7f51d3d22168691c9c08235
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-23 16:42:40 +0530

    Time: 10 ms (77.15%), Space: 18.3 MB (33.59%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,16 +1,17 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
+        from collections import deque
         n=len(senate)
-        rad=[]
-        dire=[]
+        rad=deque()
+        dire=deque()
         for i in range(len(senate)):
             if senate[i]=='R':
                 rad.append(i)
             else:
                 dire.append(i)
         while rad and dire:
-            r=rad.pop(0)
-            d=dire.pop(0)
+            r=rad.popleft()
+            d=dire.popleft()
             if r<d:
                 rad.append(r+n)
             else:
