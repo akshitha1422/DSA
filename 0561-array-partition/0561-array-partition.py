bd4c6e38057eaa62368657c4f4fea58bf32960ea
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-26 20:48:43 +0530

    Time: 30 ms (36.44%), Space: 19.8 MB (12.6%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,10 +1,9 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        # i=0
-        # total=0
-        # while i+1<len(nums):
-        #     total+=nums[i]
-        #     i=i+2
-        # return total
-        return sum(nums[::2])
+        i=0
+        total=0
+        while i+1<len(nums):
+            total+=nums[i]
+            i=i+2
+        return total
