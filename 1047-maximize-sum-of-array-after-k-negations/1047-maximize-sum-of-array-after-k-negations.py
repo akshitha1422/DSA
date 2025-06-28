74b78b204b17449429df693cd663440588b1f47e
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-28 23:59:21 +0530

    Time: 0 ms (100%), Space: 17.9 MB (61.56%) - LeetHub

---
+++ 1047-maximize-sum-of-array-after-k-negations/1047-maximize-sum-of-array-after-k-negations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i=0
+        while i<len(nums) and k>0 and nums[i]<0:
+            nums[i]=abs(nums[i])
+            i+=1
+            k-=1
+        if k%2==1:
+            nums.sort()
+            nums[0]=-nums[0]
+        return sum(nums)
