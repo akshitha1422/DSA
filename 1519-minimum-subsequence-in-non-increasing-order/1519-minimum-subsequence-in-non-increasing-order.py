b6e17bb3f9fa6641ae671275a560afbe25d645f4
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-06-29 13:49:20 +0530

    Time: 3 ms (57.59%), Space: 17.6 MB (98.88%) - LeetHub

--- 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
+++ 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
@@ -2,12 +2,14 @@ class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
         nums.sort(reverse=True)
         sub=[]
+        sum_sub=0
         total=sum(nums)
         for i in nums:
+            sum_sub+=i
             sub.append(i)
-            if sum(sub)>total-sum(sub):
+            if sum_sub>total-sum_sub:
                 return sub
-                
+
         # while sum(sub)<=total-sum(sub):
         #     sub.append(nums[i])
         #     i+=1
