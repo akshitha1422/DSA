8f74db93ad280be8918bdd394a56242581465fe0
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-07-25 16:38:17 +0530

    Time: 16 ms (53.29%), Space: 18.7 MB (34.29%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        memo={}
+        def helper(s1,s2):
+            if (s1,s2) in memo:
+                return memo[(s1,s2)]
+            if s1==s2:
+                memo[(s1,s2)]=True
+                return True
+            if len(s1)!=len(s2):
+                memo[(s1,s2)]=True
+                return False
+            if sorted(s1)!=sorted(s2):
+                memo[(s1,s2)]=False
+                return False
+            n=len(s1)
+            for i in range(1,n):
+                case1=helper(s1[i:],s2[i:]) and helper(s1[:i],s2[:i])
+                case2=helper(s1[i:],s2[:-i]) and helper(s1[:i],s2[-i:])
+                if case1 or case2:
+                    memo[(s1,s2)]=True
+                    return True
+            memo[(s1,s2)]=False
+            return False
+        return helper(s1,s2)
