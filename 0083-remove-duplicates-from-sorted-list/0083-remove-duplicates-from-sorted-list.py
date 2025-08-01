3d301b23f55055c24a60432a864a7faf92bd8b1a
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-08-01 17:53:38 +0530

    Time: 7 ms (2.47%), Space: 17.7 MB (65.51%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -17,9 +17,13 @@ class Solution:
         #     curr=curr.next
         # return head
         curr=head
-        while curr and curr.next:
-            if curr.val==curr.next.val:
-                curr.next=curr.next.next
+        prev=None
+        seen=[]
+        while curr:
+            if curr.val in seen:
+                prev.next=curr.next
             else:
-                curr=curr.next
+                seen.append(curr.val)
+                prev=curr
+            curr=curr.next
         return head
