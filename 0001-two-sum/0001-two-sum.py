0b5c2bd5bb706dc1969aba92010edc58f8430391
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-08-01 18:18:15 +0530

    Time: 1 ms (58.4%), Space: 19 MB (33.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,9 +4,10 @@ class Solution:
         for i,n in enumerate(nums):
             if (target-n) in st:
                 return [st[target-n],i]
-            st[n]=i
-
-        # st=[]
+            else:
+                st[n]=i
+        
+     # st=[]
         # for i in range(len(nums)):
         #     for j in range(i+1,len(nums)):
         #         if nums[i]+nums[j]==target:
