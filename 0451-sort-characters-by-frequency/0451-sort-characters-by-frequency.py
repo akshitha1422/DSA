c95917e2b06c484aa55bac23e6fae4425a3684ec
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-09-07 11:24:59 +0530

    Time: 3 ms (97.74%), Space: 18.9 MB (60.1%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,9 +1,20 @@
+import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
+        #same
         res=''
         from collections import Counter
         freq=Counter(s)
-        sorted_items=sorted(freq.items(),key=lambda x:x[1],reverse=True)
-        for i,v in sorted_items:
-            res+=i*v
+        st=[]
+
+        for i,v in freq.items():
+            heapq.heappush(st,(-v,i))
+        while st:
+            i,v=heapq.heappop(st)
+            res+=-i*v
         return res
+
+        # sorted_items=sorted(freq.items(),key=lambda x:x[1],reverse=True)
+        # for i,v in sorted_items:
+        #     res+=i*v
+        # return res
