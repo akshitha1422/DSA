9acb1cd80988558861e9f7bcc15d5822f116c06f
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-09-13 19:47:24 +0530

    Time: 115 ms (94.82%), Space: 46.2 MB (93.78%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        if not intervals :
+            return 0
+        if len(intervals)==1:
+            return 1
+        intervals.sort(key=lambda x:x[0])
+        heap=[]
+        heapq.heappush(heap,intervals[0][1])
+        for i in range(1,len(intervals)):
+            if heap[0]<intervals[i][0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap,intervals[i][1])
+        return len(heap)
