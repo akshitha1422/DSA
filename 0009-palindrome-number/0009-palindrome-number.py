62b7b177e1de5fb62081584296d8105f2036463d
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-09-19 21:48:20 +0530

    Time: 6 ms (66.75%), Space: 17.8 MB (39.69%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,17 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        st1=[]
-        st2=[]
-        sign=1
-        if x<0:
-            sign*=-1
-            x*=-1
-            st1.append('-')
-        while x>0:
-            a=x%10
-            st1.append(a)
-            st2.insert(0,a)
-            x=x//10
-        if sign==-1:
-            st2.append('-')
-        return st1==st2
+        s1=str(x)
+        s2=str(x)[::-1]
+        if s1==s2:
+            return True
+        else:
+            return False
