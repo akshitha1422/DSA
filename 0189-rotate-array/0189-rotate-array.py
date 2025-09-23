75ed4c1e6e104b7081248d3eedc4d321a165d0bc
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-09-23 19:53:00 +0530

    Time: 3 ms (73.84%), Space: 25.5 MB (91.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,7 +3,5 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if not nums:
-            return
         k=k%len(nums)
         nums[:]=nums[-k:]+nums[:-k]
