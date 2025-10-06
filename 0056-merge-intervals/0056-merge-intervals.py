e54ff479e456ed5f683741a20efbe8da54b2df68
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-06 21:15:41 +0530

    Time: 6 ms (78.12%), Space: 21 MB (64.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,8 +4,7 @@ class Solution:
         res=[]
         res.append(intervals[0])
         for interval in intervals:
-            arr=res[-1]
-            if interval[0]<=arr[1]:
+            if interval[0]<=res[-1][1]:
                 res[-1][0]=min(res[-1][0],interval[0])
                 res[-1][1]=max(res[-1][1],interval[1])
             else:
