c615ccb8e098a8d12ef031b11aabae74628d99cf
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-06 22:16:36 +0530

    Time: 2 ms (73.72%), Space: 19.6 MB (18.47%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res=0
-        for i in range(32):
-            bit_sum=sum((num>>i)&1 for num in nums)
-            res|=(bit_sum%3)<<i
-        if res>=2**31:
-            res-=2**32
-        return res
+        freq={}
+        for n in nums:
+            if n not in freq:
+                freq[n]=1
+            else:
+                freq[n]+=1
+        for n,f in freq.items():
+            if f==1:
+                return n
