b1775ee4c879153d6c1be4a61a6ff624d6d79f40
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-12 21:12:41 +0530

    Time: 30 ms (93.28%), Space: 28.9 MB (31.02%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -10,18 +10,26 @@ class Solution:
         left=0
         right=len(skill)-1
         res=[]
+        chem=0
         while left<right:
-            if skill[left]+skill[right]==team_sum:
-                res.append([skill[left],skill[right]])
-                left+=1
-                right-=1
-            elif skill[left]+skill[right]<team_sum:
-                left+=1
-            else:
-                right-=1
-        if len(res)!=teams:
-            return -1
-        st=0
-        for l,r in res:
-            st+=l*r
-        return st
+            if skill[left]+skill[right]!=team_sum:
+                return -1
+            chem+=skill[left]*skill[right]
+            left+=1
+            right-=1
+        return chem
+        # while left<right:
+        #     if skill[left]+skill[right]==team_sum:
+        #         res.append([skill[left],skill[right]])
+        #         left+=1
+        #         right-=1
+        #     elif skill[left]+skill[right]<team_sum:
+        #         left+=1
+        #     else:
+        #         right-=1
+        # if len(res)!=teams:
+        #     return -1
+        # st=0
+        # for l,r in res:
+        #     st+=l*r
+        # return st
