b231ff4bcaa2df3823012ce54399e4c11291938f
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-14 16:52:39 +0530

    Time: 34 ms (84.95%), Space: 17.7 MB (67.88%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        s=str(bin(n)[2:])
-        s=s.zfill(32)
-        s=s[::-1]
-        return int(s,2)
+        res=0
+        for _ in range(32):
+            res=(res<<1)|(n&1)
+            n>>=1
+        return res
+        # s=str(bin(n)[2:])
+        # s=s.zfill(32)
+        # s=s[::-1]
+        # return int(s,2)
