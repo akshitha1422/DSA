be38a9268221ac53210f1e46fbd7602adacfd4f1
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-14 18:34:55 +0530

    Time: 816 ms (40.17%), Space: 18.1 MB (87.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n=len(coins)
-        memo={}
-        def helper(amount):
-            if amount==0:
-                return 0
-            if amount<0:
-                return float('inf')
-            if amount in memo:
-                return memo[amount]
-            min_val=float('inf')
-            for c in coins:
-                res=helper(amount-c)
-                if res!=float('inf'):
-                    min_val=min(min_val,res+1)
-            memo[amount]=min_val
-            return min_val
-        result=helper(amount)
-        return result if result!=float('inf') else -1
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            for coin in coins:
+                if i-coin>=0:
+                    dp[i]=min(dp[i],dp[i-coin]+1)
+        if dp[amount]!=float('inf'):
+            return dp[amount]
+        else:
+            return -1
