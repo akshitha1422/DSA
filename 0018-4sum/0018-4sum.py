9b4ff6e9502d38d099946e97db1a2650dd291a12
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-10-14 15:15:06 +0530

    Time: 394 ms (51.79%), Space: 18.1 MB (15.68%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,27 +1,27 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        st=[]
-        for m in range(len(nums)-3):
-            if m>0 and nums[m]==nums[m-1]:
+        res=[]
+        for i in range(len(nums)-3):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            for n in range(m+1,len(nums)-2):
-                if n>m+1 and nums[n]==nums[n-1]:
+            for j in range(i+1,len(nums)-2):
+                if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                i=n+1
-                j=len(nums)-1
-                while i<j:
-                    s=nums[m]+nums[n]+nums[i]+nums[j]
+                left=j+1
+                right=len(nums)-1
+                while left<right:
+                    s=nums[i]+nums[j]+nums[left]+nums[right]
                     if s==target:
-                        st.append([nums[m],nums[n],nums[i],nums[j]])
-                        while i<j and nums[i]==nums[i+1]:
-                            i+=1
-                        while i<j and nums[j]==nums[j-1]:
-                            j-=1
-                        i+=1
-                        j-=1
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left<right and nums[left]==nums[left+1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right-1]:
+                            right-=1
+                        left+=1
+                        right-=1
                     elif s<target:
-                        i+=1
-                    elif s>target:
-                        j-=1
-        return st
+                        left+=1
+                    else:
+                        right-=1
+        return res
