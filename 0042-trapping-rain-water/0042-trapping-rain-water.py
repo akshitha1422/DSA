ef0b4bcccf793e2d2037d250e3fac8ad2900ed46
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-05 14:33:52 +0530

    Time: 1 ms (98.25%), Space: 19.2 MB (69.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,18 +3,18 @@ class Solution:
         left=0
         right=len(height)-1
         max_left=max_right=0
-        water=0
+        res=0
         while left<right:
-            if height[left]<height[right]:
+            if height[left]<=height[right]:
                 if height[left]>=max_left:
                     max_left=height[left]
                 else:
-                    water+=abs(height[left]-max_left)
+                    res+=abs(max_left-height[left])
                 left+=1
             else:
                 if height[right]>=max_right:
                     max_right=height[right]
                 else:
-                    water+=abs(height[right]-max_right)
+                    res+=abs(max_right-height[right])
                 right-=1
-        return water
+        return res
