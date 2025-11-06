a377866ea85b08d3ca916cd7923187c0c8724603
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-06 18:40:31 +0530

    Time: 5 ms (60.14%), Space: 21.4 MB (28.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
         freq=Counter(nums)
-        sorted_freq=sorted(freq.items(),key=lambda x:x[1],reverse=True)
+        sorted_freq=sorted(freq.items(),key=lambda x:x[1], reverse=True)
         return list(item[0] for item in sorted_freq[:k])
