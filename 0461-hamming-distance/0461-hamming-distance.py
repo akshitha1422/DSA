74ec3f0a933ae604498966be3061605e6b15e76b
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-06 18:07:56 +0530

    Time: 2 ms (6.85%), Space: 17.7 MB (83.58%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,4 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        res=bin(x^y)
-        return res.count('1')
+        # res=bin(x^y)
+        # return res.count('1')
+        res=0
+        while x>0 or y>0:
+            res+=(x&1)^(y&1)
+            x>>=1
+            y>>=1
+        return res
