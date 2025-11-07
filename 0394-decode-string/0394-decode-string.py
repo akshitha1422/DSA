292d7ef500cfbc9d52fc3e8dedf48aaac13559a5
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-07 15:18:29 +0530

    Time: 0 ms (100%), Space: 18 MB (12.37%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -11,9 +11,10 @@ class Solution:
                 curr=''
                 num=0
             elif i==']':
-                last, repeat = st.pop()
-                curr = last + curr * repeat
+                last,repeat=st.pop()
+                curr=last+curr*repeat
             else:
                 curr+=i
         return curr
+                    
                     
