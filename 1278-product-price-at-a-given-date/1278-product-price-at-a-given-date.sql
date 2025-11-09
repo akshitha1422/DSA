285cd49e10f97d1d04657f2e307cbf65f64cbae2
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-09 15:34:45 +0530

    Time: 748 ms (16.42%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-select distinct p1.product_id,
+select p1.product_id,
 ifnull((select p2.new_price 
 from Products as p2
 where p1.product_id=p2.product_id and change_date<='2019-08-16'
 order by change_date desc 
 limit 1),10) as price
-from (select product_id from Products) as p1
+from (select distinct product_id from Products) as p1
