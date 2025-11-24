cf2d879a1ecb17545320962e77d10ee23e424168
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-24 16:56:26 +0530

    Time: 59 ms (95.11%), Space: 51.4 MB (77.25%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,12 +1,21 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort(key=lambda x:x[1])
-        res=[]
-        res.append(points[0])
+        count=1
+        prev=points[0][1]
         for point in points:
-            if res[-1][1]>=point[0]:
-                res[-1][0]=min(point[0],res[-1][0])
-                res[-1][1]=min(point[1],res[-1][1])
-            else:
-                res.append(point)
-        return len(res)
+            if point[0]>prev:
+                count+=1
+                prev=point[1]
+        return count
+                
+        # points.sort(key=lambda x:x[1])
+        # res=[]
+        # res.append(points[0])
+        # for point in points:
+        #     if res[-1][1]>=point[0]:
+        #         res[-1][0]=min(point[0],res[-1][0])
+        #         res[-1][1]=min(point[1],res[-1][1])
+        #     else:
+        #         res.append(point)
+        # return len(res)
