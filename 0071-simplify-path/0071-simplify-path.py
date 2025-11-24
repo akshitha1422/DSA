cca984765ba9c64f690b1c6ff36c844d57205009
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2025-11-24 17:20:33 +0530

    Time: 4 ms (16.16%), Space: 18.1 MB (13.17%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        while path and path[-1]=='/':
+            path=path[:-1]
+        st=[]
+        path=path.split('/')
+        for i in path:
+            if i=='' or i=='.':
+                continue
+            elif i=='..':
+                if st:
+                    st.pop()
+            else:
+                st.append(i)
+        return '/'+'/'.join(st)
