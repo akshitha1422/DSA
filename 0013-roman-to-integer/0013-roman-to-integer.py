43f0d12bbc1729daa41efd4528c787082d48c98b
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2026-03-14 19:59:34 +0530

    Time: 3 ms (80.34%), Space: 19.3 MB (84.5%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,14 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        total=0
-        sym={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-        i=0
-        while i<len(s):
-            if i<(len(s)-1) and (s[i]+s[i+1]) in sym.keys():
-                total+=sym[s[i]+s[i+1]]
-                i+=2
+        sym={'I':1,'IV':4,'V':5,'IX':9,'X':10,'XL':40,'L':50,'XC':90,'C':100,'CD':400,'D':500,'CM':900,'M':1000}
+        res=0
+        while len(s)>0:
+            if s[:2] in sym:
+                res+=sym[s[:2]]
+                s=s[2:]
             else:
-                total+=sym[s[i]]
-                i+=1
-        return total
-    
+                res+=sym[s[0]]
+                s=s[1:]
+        return res
