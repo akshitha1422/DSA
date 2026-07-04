cac677c58923d95c766ade073f9dab8c46c14864
Akshitha Reddy <ugs22075_it.akshitha@cbit.org.in>
2026-07-04 15:24:58 +0530

    Time: 1401 ms (6.76%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -5,4 +5,4 @@ cross join subjects as u
 left join Examinations as e
 on s.student_id=e.student_id and u.subject_name=e.subject_name
 group by s.student_id, s.student_name, u.subject_name
-order by s.student_id, s.student_name, u.subject_name
+order by s.student_id, u.subject_name
